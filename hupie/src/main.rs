10d7db69b6f9742e49a76c77e357fd00563cdccd
808Mak1r <39427457+808Mak1r@users.noreply.github.com>
2021-12-17 17:20:31 +0800

    update

--- hupie/src/main.rs
+++ hupie/src/main.rs
@@ -1,4 +1,15 @@
 use clap::Parser;
+use anyhow::{Result, anyhow};
+use reqwest::{Url, header, Client, Response};
+use std::{str::FromStr, collections::HashMap};
+use colored::*;
+use mime::Mime;
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
 
 
 /// echo bef json
@@ -19,21 +30,159 @@ enum Subcommand {
 #[derive(Debug, Parser)]
 struct Get {
     /// http get url
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
 }
 
 /// post with an url data as JSON
 #[derive(Debug, Parser)]
 struct Post {
     /// http post url
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
     /// http post body
-    body: Vec<String>,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
+}
+
+/// key=value parse_kv_pair as KvPair
+#[derive(Debug, PartialEq)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("parse失败 {}", s));
+        Ok(Self { 
+            k: (split.next().ok_or_else(err)?).to_string(), 
+            v: (split.next().ok_or_else(err)?).to_string(),
+    })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{:?}", status);
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    print!("\n");
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+        _ => println!("{}", body),
+    }
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
 }
 
 
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts)
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Hupie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        Subcommand::Get(ref args) => get(client, args).await?,
+        Subcommand::Post(ref args) => post(client, args).await?,
+    };
+    
+    Ok(result)
+}
+
+
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into()
+            }
+        );
+    }
 }
