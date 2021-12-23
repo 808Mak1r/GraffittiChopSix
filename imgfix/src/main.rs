47ad32dee0bf11be403436ccab60a545063dd6c3
808Mak1r <39427457+808Mak1r@users.noreply.github.com>
2021-12-23 10:49:56 +0800

    update

--- imgfix/src/main.rs
+++ imgfix/src/main.rs
@@ -1,4 +1,21 @@
 
+struct ImageSpec {
+    specs: Vec<Spec>,
+}
+
+enum Spec {
+    Resize(Resize),
+    Crop(Crop),
+}
+
+struct Resize {
+    width: u32,
+    height: u32,
+}
+
+
+
+
 fn main() {
     prost_build::Config::new()
         .out_dir("src/pb")
